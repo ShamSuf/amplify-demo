b217bdb3f1658e5574b0bd2ac91402a68d2f644c
ShamSuf <shamoun.sufyan@gmail.com>
2020-10-13 15:03:07 +1100

    Update App.js
    update develop branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Shamoun FINAL
+           Shamoun - this is a develop branch
           </p>
           <a
             className="App-link"
